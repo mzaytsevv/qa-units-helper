970deb414de6cc3858bcdfc4aa8525041d9972ad
JoseLVelas <jose.l.velasquez@gmail.com>
2022-05-07 08:19:05 +0900

    Clean up

--- create_e2e.py
+++ create_e2e.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import csv
-import time
 
 from jira import JIRA
 
@@ -58,8 +57,9 @@ def e2e_tickets(data):
                 line_json[outgoing_header] = outgoing_value
             elif incoming_header == "Outward issue link (Tests)":
                 outgoing_value = line[num_column]
+                print(f" outgoing_value: {outgoing_value}, num_column= {num_column}")
                 if outgoing_value != "":
-                    links.append(line[num_column])
+                    links.append(outgoing_value)
             num_column = num_column + 1
         line_with_links = [line_json, links]
         tickets.append(line_with_links)
@@ -70,17 +70,18 @@ def create_tickets_and_links(tickets_with_links):
     for ticket_with_links in tickets_with_links:
         try:
             ticket = ticket_with_links[0]
-            issue_created = jira.create_issue(fields=ticket)
-            print(f"{issue_created.key}")
+            #issue_created = jira.create_issue(fields=ticket)
+            #print(f"{issue_created.key}")
+            print(f"============== new ticket")
             try:
                 tickets_to_link = ticket_with_links[1]
                 for ticket_to_link in tickets_to_link:
                     ticket_to_link = ticket_to_link.strip()
-                    issue_link = jira.create_issue_link(type='tests', inwardIssue=issue_created.key,
-                                                        outwardIssue=ticket_to_link)
-                    print(f"/t'tests' -> {ticket_to_link}")
+                    #issue_link = jira.create_issue_link(type='tests', inwardIssue=issue_created.key,
+                    #                                    outwardIssue=ticket_to_link)
+                    print(f"\t'tests' -> {ticket_to_link}")
             except Exception as e:
-                print(f"Error while linking the issue '{issue_created.key}' to '{ticket_to_link}'. Exception: {e}")
+                print(f"Error while linking the issue '{issue_created.key}' to '{ticket_to_link}'. \n\tException: {e}")
         except Exception as e:
             print(f"Error while creating the issue: {ticket}. Exception: {e}")
 
